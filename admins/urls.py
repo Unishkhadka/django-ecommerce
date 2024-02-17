2178b530f18a4bcd75ad9375abc2eb99747ce7f8
Unish@14 <khadkaunish321@gmail.com>
2024-02-17 13:48:30 +0545

    added sidebar

--- admins/urls.py
+++ admins/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("admin-dashboard/", dashboard, name="dashboard"),
+    path("", dashboard, name="dashboard"),
+    path("orders/", orders, name="orders"),
+    path("admin-products/", admin_products, name="admin-products"),
+    path("customers/", customers, name="customers"),
     ]
--- admins/views.py
+++ admins/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+
 def dashboard(request):
     return render(request, "admins/dashboard.html")
+
+
+def orders(request):
+    return render(request, "admins/orders.html")
+
+
+def admin_products(request):
+    return render(request, "admins/admin-products.html")
+
+
+def customers(request):
+    return render(request, "admins/customers.html")
